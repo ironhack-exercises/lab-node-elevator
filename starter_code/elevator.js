e910eb57bb42d08dbacf4dceff8a81f7cd4144b5
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-04 19:31:23 +0200

    Iteration 4: Passengers enter and passengers leave the elevator

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,9 +1,11 @@
 class Elevator {
   constructor(){
-    this.floor      = 0
-    this.MAXFLOOR   = 10
-    this.requests   = []
-    this.direction  = 'up'
+    this.floor       = 0
+    this.MAXFLOOR    = 10
+    this.requests    = []
+    this.waitingList = []
+    this.passengers  = []
+    this.direction   = 'up'
     this.intervalId
   }
 
@@ -12,26 +14,55 @@ class Elevator {
   }
 
   stop() {
-    setTimeout(()=> clearInterval(this.intervalId), 5000)
+    clearInterval(this.intervalId)
   }
 
   update() {
     this.log()
+    if (this.requests.length == 0) this.stop()
+    else {
+      if(this.requests[0] == this.floor){
+        this.requests.shift()
+        this._passengersLeave()
+        this._passengersEnter()
+      }
+      else
+        this.requests[0] > this.floor ? this.floorUp() : this.floorDown()
+    }
   }
 
-  _passengersEnter() { }
-  _passengersLeave() { }
+  _passengersEnter() {
+    this.waitingList.forEach(e => {
+      if(e.originFloor == this.floor){
+         this.passengers.push(e)
+         this.requests.push(e.destinationFloor)
+         console.log(`${e.name} has enter the elevator`)
+       }
+    })
+    this.waitingList = this.waitingList.filter(e => e.originFloor != this.floor)
+  }
+
+  _passengersLeave() {
+    this.passengers.forEach(e => {
+      if(e.destinationFloor == this.floor)
+         console.log(`${e.name} has left the elevator`)
+    })
+    this.passengers = this.passengers.filter(e => e.destinationFloor != this.floor)
+  }
 
   floorUp() {
+    this.direction = 'up'
     if(this.floor < this.MAXFLOOR) this.floor++
   }
 
   floorDown() {
+    this.direction = 'down'
     if(this.floor > 0) this.floor--
   }
 
   call(person) {
-    this.requests.push(person)
+    this.requests.push(person.originFloor)
+    this.waitingList.push(person)
   }
 
   log() {
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,18 +1,16 @@
 const Elevator = require('./elevator.js')
-const Person = require('./elevator.js')
+const Person = require('./person.js')
 
 let elevator = new Elevator()
-// elevator.start()
-// elevator.stop()
+elevator.start()
 
-for(let i=0; i<=11; i++){
-  elevator.floorUp()
-  elevator.log()
-}
+let julia = new Person ('Julia', 2, 6)
+let maria = new Person ('Maria', 1, 5)
+let ana = new Person ('Ana', 5, 2)
+let pedro = new Person ('Pedro', 6, 1)
 
-console.log('-------------');
 
-for(let i=0; i<=11; i++){
-  elevator.floorDown()
-  elevator.log()
-}
+elevator.call(julia)
+elevator.call(pedro)
+setTimeout(()=> elevator.call(maria), 2000)
+setTimeout(()=> elevator.call(ana), 3000)
